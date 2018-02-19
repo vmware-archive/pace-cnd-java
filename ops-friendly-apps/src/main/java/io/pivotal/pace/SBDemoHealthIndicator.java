40c79d107d9a4db0100dff9574385fba789d8955
James Williams <jwilliams@pivotal.io>
2018-02-19 16:01:58 -0500

    finished converting ops-friendly-apps demo to new format

---
+++ ops-friendly-apps/src/main/java/io/pivotal/pace/SBDemoHealthIndicator.java
@@ -0,0 +1,22 @@
+package io.pivotal.pace;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SBDemoHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        int errorCode = check(); // perform some specific health check
+        if (errorCode != 0) {
+            return Health.down().withDetail("Error code", errorCode).build();
+        }
+        return Health.up().build();
+    }
+
+    private int check() {
+    		return (int)(Math.random()*2);
+    }
+}
